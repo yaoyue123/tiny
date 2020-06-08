846587989c779d87e61354965cf77f65aabf6679
yaoyue123 <2742949404@qq.com>
2020-06-08 22:09:18 +0800

    add int before main() fuct

--- src/MAIN.C
+++ src/MAIN.C
@@ -32,9 +32,9 @@
 
 /* allocate global variables */
 int lineno = 0;
-FILE * source;
-FILE * listing;
-FILE * code;
+FILE *source;
+FILE *listing;
+FILE *code;
 
 /* allocate and set tracing flags */
 int EchoSource = FALSE;
@@ -45,53 +45,64 @@ int TraceCode = FALSE;
 
 int Error = FALSE;
 
-main( int argc, char * argv[] )
-{ TreeNode * syntaxTree;
+int main(int argc, char *argv[])
+{
+  TreeNode *syntaxTree;
   char pgm[120]; /* source code file name */
   if (argc != 2)
-    { fprintf(stderr,"usage: %s <filename>\n",argv[0]);
-      exit(1);
-    }
-  strcpy(pgm,argv[1]) ;
-  if (strchr (pgm, '.') == NULL)
-     strcat(pgm,".tny");
-  source = fopen(pgm,"r");
-  if (source==NULL)
-  { fprintf(stderr,"File %s not found\n",pgm);
+  {
+    fprintf(stderr, "usage: %s <filename>\n", argv[0]);
+    exit(1);
+  }
+  strcpy(pgm, argv[1]);
+  if (strchr(pgm, '.') == NULL)
+    strcat(pgm, ".tny");
+  source = fopen(pgm, "r");
+  if (source == NULL)
+  {
+    fprintf(stderr, "File %s not found\n", pgm);
     exit(1);
   }
   listing = stdout; /* send listing to screen */
-  fprintf(listing,"\nTINY COMPILATION: %s\n",pgm);
+  fprintf(listing, "\nTINY COMPILATION: %s\n", pgm);
 #if NO_PARSE
   TokenType prev_token = ENDFILE;
-  while (getToken(&prev_token)!=ENDFILE);
+  while (getToken(&prev_token) != ENDFILE)
+    ;
 #else
   syntaxTree = parse();
-  if (TraceParse) {
-    fprintf(listing,"\nSyntax tree:\n");
+  if (TraceParse)
+  {
+    fprintf(listing, "\nSyntax tree:\n");
     printTree(syntaxTree);
   }
 #if !NO_ANALYZE
-  if (! Error)
-  { if (TraceAnalyze) fprintf(listing,"\nBuilding Symbol Table...\n");
+  if (!Error)
+  {
+    if (TraceAnalyze)
+      fprintf(listing, "\nBuilding Symbol Table...\n");
     buildSymtab(syntaxTree);
-    if (TraceAnalyze) fprintf(listing,"\nChecking Types...\n");
+    if (TraceAnalyze)
+      fprintf(listing, "\nChecking Types...\n");
     typeCheck(syntaxTree);
-    if (TraceAnalyze) fprintf(listing,"\nType Checking Finished\n");
+    if (TraceAnalyze)
+      fprintf(listing, "\nType Checking Finished\n");
   }
 #if !NO_CODE
-  if (! Error)
-  { char * codefile;
-    int fnlen = strcspn(pgm,".");
-    codefile = (char *) calloc(fnlen+4, sizeof(char));
-    strncpy(codefile,pgm,fnlen);
-    strcat(codefile,".tm");
-    code = fopen(codefile,"w");
+  if (!Error)
+  {
+    char *codefile;
+    int fnlen = strcspn(pgm, ".");
+    codefile = (char *)calloc(fnlen + 4, sizeof(char));
+    strncpy(codefile, pgm, fnlen);
+    strcat(codefile, ".tm");
+    code = fopen(codefile, "w");
     if (code == NULL)
-    { printf("Unable to open %s\n",codefile);
+    {
+      printf("Unable to open %s\n", codefile);
       exit(1);
     }
-    codeGen(syntaxTree,codefile);
+    codeGen(syntaxTree, codefile);
     fclose(code);
   }
 #endif
@@ -100,4 +111,3 @@ main( int argc, char * argv[] )
   fclose(source);
   return 0;
 }
-
